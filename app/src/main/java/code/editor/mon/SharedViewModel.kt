1fc9c85b3e31bbbdd62366d989abd2542431c0bc
nizarardhian1-tech <nizarardhian1@gmail.com>
2026-04-29 00:21:12 +0700

    chore: clear repo contents [skip ci]

--- app/src/main/java/code/editor/mon/SharedViewModel.kt
+++
@@ -1,9 +0,0 @@
-package code.editor.mon
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class SharedViewModel : ViewModel() {
-    val openFileRequest = MutableLiveData<FileOpenRequest?>()
-    val projectLoaded = MutableLiveData<Boolean>()
-}
